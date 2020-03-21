41e0c0508fa358d2dc9fb557daca99091823368c
Alexey Zhelezov <zhelezovalexey@gmail.com>
2020-03-21 18:23:26 +0100

    20 minutos hecho

--- mosto.py
+++ mosto.py
@@ -19,53 +19,73 @@ noticias = [{}]
 
 def scrapeElMundo():
     print('el mundo')
-    r = requests.get('https://www.elmundo.es/ciencia-y-salud/salud.html')
-    webContent = r.text
-    soup = BeautifulSoup(webContent)
-    articles = soup.find_all('article')
-    for article in articles:
-        for header in article.find_all('header'):
-            print(header.text)
-            link = header.find('a')['href']
-            result = requests.get(link)
-            soup = BeautifulSoup(result.text)
-            article = soup.find('article')
-            articleBody = article.find(
-                'div', {'class': 'ue-l-article__body ue-c-article__body'})
+    categorias = [
+        {'name': 'Salud', 'endpoint': 'ciencia-y-salud/salud.html'},
+        {'name': 'Ciencia', 'endpoint': 'ciencia-y-salud/ciencia.html'},
+        {'name': 'Tecnologia', 'endpoint': 'tecnologia.html'}
+    ]
 
-            if articleBody is None:
-                continue
-            # Unwanted tags
-            unwantedDiv = articleBody.find(
-                'div', {'class': 'ue-c-article__trust'})
-            unwantedUl = articleBody.find('ul')
-            unwantedPremium = articleBody.find(
-                'div', {'class': 'ue-c-article__premium'})
-            unwantedDivRelated = articleBody.find(
-                'div', {'class': 'ue-c-article__related-news ue-l-article--top-separator'})
-            unwantedDiv.extract()
+    for categoria in categorias:
+        r = requests.get(f'https://www.elmundo.es/{categoria["endpoint"]}')
+        webContent = r.text
+        soup = BeautifulSoup(webContent)
+        articles = soup.find_all('article')
+        for article in articles:
+            for header in article.find_all('header'):
+                print(header.text)
+                link = header.find('a')['href']
+                result = requests.get(link)
+                soup = BeautifulSoup(result.text)
+                article = soup.find('article')
+                articleBody = article.find(
+                    'div', {'class': 'ue-l-article__body ue-c-article__body'})
 
-            # try catch blocks no entiendo
-            try:
-                unwantedUl.extract()
-            except:
-                print('no ul')
-
-            try:
+                if articleBody is None:
+                    continue
+                # Unwanted tags
+                unwantedDiv = articleBody.find(
+                    'div', {'class': 'ue-c-article__trust'})
+                unwantedUl = articleBody.find('ul')
+                unwantedPremium = articleBody.find(
+                    'div', {'class': 'ue-c-article__premium'})
+                unwantedDivRelated = articleBody.find(
+                    'div', {'class': 'ue-c-article__related-news ue-l-article--top-separator'})
                 unwantedDiv.extract()
-            except:
-                print('no trust')
 
-            try:
-                unwantedPremium.extract()
-            except:
-                print('no premium')
+                # try catch blocks no entiendo
+                try:
+                    unwantedUl.extract()
+                except:
+                    print('no ul')
 
-            try:
-                unwantedDivRelated.extract()
-            except:
-                print('no related')
-            print(articleBody.text)
+                try:
+                    unwantedDiv.extract()
+                except:
+                    print('no trust')
+
+                try:
+                    unwantedPremium.extract()
+                except:
+                    print('no premium')
+
+                try:
+                    unwantedDivRelated.extract()
+                except:
+                    print('no related')
+                
+                dateTime = article.find('time')['datetime']
+                doc = {
+                    'title': header.text,
+                    'categoria': categoria['name'],
+                    'noticia': articleBody.text,
+                    'fecha': dateTime
+                }
+                jsonDoc = json.dumps(doc)
+                path = os.getcwd()
+                f = open(path + f'/El Mundo/{categoria["name"]}/{categoria["name"]}.{dateTime}.txt', 'w+', encoding='utf-8')
+                f.writelines(jsonDoc)
+                f.close()
+
 
 
 def scrapeElPais():
@@ -112,6 +132,51 @@ def scrapeElPais():
 
 def scrape20Minutos():
     print('20 minutos')
+    categorias = [
+        {'name': 'Salud', 'endpoint': 'salud/'},
+        {'name': 'Ciencia', 'endpoint': 'ciencia/'},
+        {'name': 'Tecnologia', 'endpoint': 'tecnologia/'}
+    ]
+
+    for categoria in categorias:
+        r = requests.get(f'https://www.20minutos.es/{categoria["endpoint"]}')
+        soup = BeautifulSoup(r.text, 'html.parser')
+        articles = soup.find_all('article')
+        for article in articles:
+            titleContainer = article.find('div', { 'class': 'media-content' })
+            hyperLink = titleContainer.find('a')
+            articleLink = hyperLink['href']
+            articleTitle = hyperLink.text
+            titleClean = articleTitle.strip()
+            print(titleClean)
+            articlePage = requests.get(articleLink)
+            parsedPage = BeautifulSoup(articlePage.text, 'html.parser')
+            dateTime = parsedPage.find('span', { 'class': 'article-date' })
+            if dateTime is None:
+                continue
+
+            articleSection = parsedPage.find('article', { 'class': 'article-body' })
+            if articleSection is None:
+                continue
+
+            articleContent = articleSection.find('div', { 'class': 'article-text' })
+            if articleContent is None:
+                continue
+
+            noticia = articleContent.text
+
+            doc = {
+                'title': titleClean,
+                'categoria': categoria['name'],
+                'noticia': noticia,
+                'fecha': dateTime.text
+            }
+            jsonDoc = json.dumps(doc)
+            path = os.getcwd()
+            f = open(path + f'/20 Minutos/{categoria["name"]}/{categoria["name"]}.{dateTime.text}.txt', 'w+')
+            f.writelines(jsonDoc)
+            f.close()
+
 
 
 def createDirectories():
@@ -131,8 +196,8 @@ def createDirectories():
 
 def scrapeAll():
     createDirectories()
-    # scrapeElMundo()
-    scrapeElPais()
+    #scrapeElMundo()
+    #scrapeElPais()
     scrape20Minutos()
 
 
